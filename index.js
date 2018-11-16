587e390f4f9431210250dd97a208b1fcf4d4a346
Shemar Dacosta <dshemar6@gmail.com>
2018-11-16 19:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ console.log(add(1,1,1))
 function happyHoldays(name){
   return `Happy Holidays, ${name}!`
 }
+
+function happyHoldaysTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, day){
+  return `It's ${days} days until ${holiday}!`
+}
