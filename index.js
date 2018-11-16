f5e8eae6521a51ca39959dbfc6f61792f8e4f7ce
Shemar Dacosta <dshemar6@gmail.com>
2018-11-16 19:58:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function happyHolidays(){
   return `Happy holidays!`
 }
 
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
